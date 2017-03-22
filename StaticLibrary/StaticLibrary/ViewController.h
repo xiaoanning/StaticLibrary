81f497f34f4c102f9496f34dfd3a2f0d54d4d25e
xiaoanning <734871221@qq.com>
2017-03-22 08:52:48 +0800

    1

---
+++ StaticLibrary/StaticLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticLibrary
+//
+//  Created by 安宁 on 17/3/22.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
